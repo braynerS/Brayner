cee0212f25702f33f8a8d40cf77deea8cde3d75b
brayner <brayner1112@gmail.com>
2021-11-08 11:03:57 -0500

    ejemplo

---
+++ matematica.php
@@ -0,0 +1,20 @@
+<?php
+
+class matbasica{
+    public $valor1=0;
+    public $valor2=0;
+    public $total1=0;
+
+
+public function __contruct($ivalor1,$ivalor2)
+{
+    $this-valor1 - $ivalor1;
+    $this-valor1 - $ivalor2;
+}
+public function leersuma()
+{
+    $this->tota1 =$this->valor1 + $this->valor2;
+    return $this->tota1;
+}
+}
+?>
